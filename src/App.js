340056f5b44974ff171ddf5dc4941770b49676fb
sreeramadasuarun <110899390+sreeramadasuarun@users.noreply.github.com>
2022-12-06 09:42:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,131 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import backward from "./assets/backward.png"
+import play from "./assets/play.png"
+import forward from "./assets/forward.png"
+import pause from "./assets/pause.svg"
+import { useState, useRef } from "react";
+import KGF from "./assets/KGF4.mp4"
+
 
 function App() {
+
+
+
+  const videoRef = useRef(null);
+  const [playing, setPlaying] = useState(false);
+  const [videoTime, setVideoTime] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [progress, setProgress] = useState(0);
+
+
+
+  const videoHandler = (control) => {
+    if (control === "play") {
+      videoRef.current.play();
+      setPlaying(true);
+    } else if (control === "pause") {
+      videoRef.current.pause();
+      setPlaying(false);
+    }
+
+
+    if (control === "play") {
+      videoRef.current.play();
+      setPlaying(true);
+      var vid = document.getElementById("video1");
+      setVideoTime(vid.duration);
+    }
+
+  };
+
+
+
+  const fastForward = () => {
+    videoRef.current.currentTime += 5;
+  };
+
+  const revert = () => {
+    videoRef.current.currentTime -= 5;
+  };
+
+
+  window.setInterval(function () {
+    setCurrentTime(videoRef.current?.currentTime);
+    setProgress((videoRef.current?.currentTime / videoTime) * 100);
+  }, 1000);
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+
+    <div className="app" style={{ position: "initial", width: "50%", height: "50%" }}>
+
+      <video
+        id="video1"
+        ref={videoRef}
+        className="movie"
+        src={KGF}>
+
+      </video>
+
+      <div className="controlsContainer">
+        <div className="controls">
+
+
+
+          <img onClick={revert} className="controlsIcon" alt="" src={backward} />
+
+
+          {playing ? (
+            <img
+              onClick={() => videoHandler("pause")}
+              className="controlsIcon--small"
+              alt=""
+              src={pause}
+            />
+          ) : (
+            <img
+              onClick={() => videoHandler("play")}
+              className="controlsIcon--small"
+              alt=""
+              src={play}
+            />
+          )}
+
+
+
+          <img onClick={fastForward} className="controlsIcon" alt="" src={forward} />
+
+
+        </div>
+      </div>
+
+
+      <div className="timecontrols">
+        <p className="controlsTime">
+          {Math.floor(videoTime / 60) + ":" + ("0" + Math.floor(videoTime % 60)).slice(-2)}
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <div className="time_progressbarContainer">
+          <div
+            style={{ width: `${progress}%` }}
+            className="time_progressBar"
+          ></div>
+        </div>
+
+
+        <p className="controlsTime">
+          {Math.floor(currentTime / 60) + ":" + ("0" + Math.floor(currentTime % 60)).slice(-2)}
+
+        </p>
+      </div>
+
     </div>
-  );
+  )
 }
 
 export default App;
